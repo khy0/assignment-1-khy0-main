c7df774f10bddac38b50b0bfe0577a52df7704c6
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-16 01:27:05 +1300

    Initial commit

---
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Main.PolicyType;
+
+public class InsuranceSystem {
+  public InsuranceSystem() {
+    // Only this constructor can be used (if you need to initialise fields).
+  }
+
+  public void printDatabase() {
+    // TODO Complete this method
+    MessageCli.PRINT_DB_POLICY_COUNT.printMessage();
+  }
+
+  public void createNewProfile(String userName, String age) {
+    // TODO: Complete this method.
+  }
+
+  public void loadProfile(String userName) {
+    // TODO: Complete this method.
+  }
+
+  public void unloadProfile() {
+    // TODO: Complete this method.
+  }
+
+  public void deleteProfile(String userName) {
+    // TODO: Complete this method.
+  }
+
+  public void createPolicy(PolicyType type, String[] options) {
+    // TODO: Complete this method.
+  }
+}
