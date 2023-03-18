6914216aa6d368f67640d3c7be02fa00b3e99d80
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-18 20:21:40 +1300

    Adding codestyle file and new method in Database

--- src/main/java/nz/ac/auckland/se281/Database.java
+++ src/main/java/nz/ac/auckland/se281/Database.java
@@ -1,5 +1,8 @@
 package nz.ac.auckland.se281;
+import java.util.ArrayList;
 
 public class Database {
-    
+    public void createNewProfile(String userName, String age) {
+        ArrayList<String> dbList = new ArrayList<String>(); 
+      }
 }
