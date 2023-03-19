6b4bfecd0d151bb66aada64c372497a09cac5a6b
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-19 15:47:52 +1300

    Adding a unique method for all strings in database

--- src/main/java/nz/ac/auckland/se281/Database.java
+++ src/main/java/nz/ac/auckland/se281/Database.java
@@ -1,22 +1,32 @@
 package nz.ac.auckland.se281;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Database {
-    
-    public void storeInDatabase(String userName, String age) {
-        Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> userList = new ArrayList<>();
-        ArrayList<Integer> ageList = new ArrayList<>();
+  public void storeInDatabase(String userName, String age) {
+    Scanner sc = new Scanner(System.in);
 
-        String userInput = sc.nextLine();
-        userList.add(userInput);
-        Integer ageInput = sc.nextInt();
-        ageList.add(ageInput);
+    ArrayList<String> userList = new ArrayList<>();
+    ArrayList<Integer> ageList = new ArrayList<>();
 
-        String[] userDatabase = userList.toArray(new String[0]);
-        Integer[] ageDatabase = ageList.toArray(new Integer[0]);
+    String userInput = sc.nextLine();
+    userList.add(userInput);
+    Integer ageInput = sc.nextInt();
+    ageList.add(ageInput);
+
+    String[] userDatabase = userList.toArray(new String[0]);
+    Integer[] ageDatabase = ageList.toArray(new Integer[0]);
+  }
+
+  public void unique(String[] userDatabase) {
+    Set<String> set = new HashSet<>();
+
+    for (String element : userDatabase) {
+      set.add(element);
     }
+  }
 }
