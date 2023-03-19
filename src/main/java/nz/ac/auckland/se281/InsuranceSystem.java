8e647972cd8f45d5103710cb15f190e632919b96
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-19 18:11:05 +1300

    Calling storeInDatabase inside InsuranceSystem

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -10,18 +10,12 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
-  
+    
   }
 
   public void createNewProfile(String userName, String age) {
-    Scanner sc = new Scanner(System.in);
-    ArrayList<String> userList = new ArrayList<>();
-
-    String userInput = sc.nextLine();
-    userList.add(userInput);
-
-    String[] userDatabase = userList.toArray(new String[0]);
-    
+    Database obj = new Database();
+    obj.storeInDatabase(userName, age);
   }
 
   public void loadProfile(String userName) {
