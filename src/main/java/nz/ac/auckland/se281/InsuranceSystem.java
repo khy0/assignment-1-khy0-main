e93c0d5482ae5b0d762ed2ffbd597338de081ef0
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-21 18:14:04 +1300

    Fixed issue with printDatabase()

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -16,11 +16,11 @@ public class InsuranceSystem {
 
   public void printDatabase() {
     ArrayList<String> userDatabase = obj.getUserDatabase();
-    ArrayList<String> ageDatabase = obj.getUserDatabase();
+    ArrayList<String> ageDatabase = obj.getAgeDatabase();
     // System.out.printf("Database has %s profile%s%s");
 
     for (int i = 0; i < userDatabase.size(); i++) {
-      System.out.printf("%d: %s, %n", i + 1, userDatabase.get(i), ageDatabase.get(i));
+      System.out.printf("%d: %s, %s %n", i + 1, userDatabase.get(i), ageDatabase.get(i));
     }
   }
 
