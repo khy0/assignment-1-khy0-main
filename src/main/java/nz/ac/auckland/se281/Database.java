e51cbc26dcb01f7168a71bbdc42c9ce5894bdc49
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-03-22 15:46:28 +1300

    Adding comments and formatting the code

--- src/main/java/nz/ac/auckland/se281/Database.java
+++ src/main/java/nz/ac/auckland/se281/Database.java
@@ -1,10 +1,7 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-// import java.util.Arrays;
 import java.util.HashSet;
-// import java.util.Set;
-// import org.apache.commons.text.WordUtils;;
 
 public class Database {
 
@@ -12,6 +9,7 @@ public class Database {
   private ArrayList<String> ageDatabase;
 
   public Database() {
+    // Defining ArrayLists for user and age
     userDatabase = new ArrayList<>();
     ageDatabase = new ArrayList<>();
   }
@@ -25,34 +23,40 @@ public class Database {
   }
 
   public void storeInDatabase(String userName, String age) {
+    // Processes the string in title case
     userName = userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
+    
+    // Adds the userName and age to the database ArrayLists
     userDatabase.add(userName);
     ageDatabase.add(age);
-    
+
+    // Defining a HashSet to make a unique set of strings inside the user database
     HashSet<String> uniqueSet = new HashSet<String>(userDatabase);
+
+    // Converting age string to age integer
     int ageInt = Integer.parseInt(age);
 
+    // Checking if the string is unique, has more than 3 characters and the age is a positive integer
     if (uniqueSet.size() == userDatabase.size() && userName.length() >= 3 && ageInt > 0) {
       System.out.printf("New profile created for %s with age %s.", userName, age);
-    }
-    else if (uniqueSet.size() != userDatabase.size()){
+    } else if (uniqueSet.size() != userDatabase.size()) {
       userDatabase.remove(userName);
       ageDatabase.remove(age);
       System.out.printf("Usernames must be unique. No profile was created for '%s'.", userName);
-    }
-    else if (userName.length() < 3) {
+    } else if (userName.length() < 3) {
       userDatabase.remove(userName);
       ageDatabase.remove(age);
-      System.out.printf("'%s' is an invalid username, it should be at least 3 characters long. No profile was"
-      + " created.", userName);
-    }
-    else if (ageInt < 0) {
+      System.out.printf(
+          "'%s' is an invalid username, it should be at least 3 characters long. No profile was"
+              + " created.",
+          userName);
+    } else if (ageInt < 0) {
       userDatabase.remove(userName);
       ageDatabase.remove(age);
-      System.out.printf("'%s' is an invalid age, please provide a positive whole number only. No profile was created"
-      + " for %s.", age, userName);
+      System.out.printf(
+          "'%s' is an invalid age, please provide a positive whole number only. No profile was"
+              + " created for %s.",
+          age, userName);
     }
-    
   }
-
 }
--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,11 +1,6 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-// import java.util.Arrays;
-// import java.util.HashSet;
-// import java.util.Set;
-// import java.util.ArrayList;
-// import java.util.Scanner;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
@@ -18,9 +13,11 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
+    // Calling both userDatabase and ageDatabase arrays
     ArrayList<String> userDatabase = obj.getUserDatabase();
     ArrayList<String> ageDatabase = obj.getAgeDatabase();
 
+    // Checks how many profiles there are in the database and prints it
     if (userDatabase.size() == 0) {
       System.out.printf("Database has %s profile%s%s%n", 0, "s", ".");
     } else if (userDatabase.size() == 1) {
@@ -29,12 +26,14 @@ public class InsuranceSystem {
       System.out.printf("Database has %s profile%s%s%n", userDatabase.size(), "s", ":");
     }
 
+    // Loops through the ArrayList and prints both the user and age databases
     for (int i = 0; i < userDatabase.size(); i++) {
       System.out.printf("%d: %s, %s%n", i + 1, userDatabase.get(i), ageDatabase.get(i));
     }
   }
 
   public void createNewProfile(String userName, String age) {
+    // Calling previously made function from Database class
     obj.storeInDatabase(userName, age);
   }
 
