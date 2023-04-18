c9cd4ad2b58bf98e5905d1bd35fe990814b6b54f
Sebasatian <rtor313@aucklanduni.ac.nz>
2023-04-18 23:01:29 +1200

    Base premium calculation for Home sub class

--- src/main/java/nz/ac/auckland/se281/Home.java
+++ src/main/java/nz/ac/auckland/se281/Home.java
@@ -1,13 +1,20 @@
 package nz.ac.auckland.se281;
 
 public class Home extends Policy {
+    int basePremium;
+    int sumInsured = Integer.parseInt(options[0]);
+    boolean isRented = Boolean.parseBoolean(options[2]);
+
     public Home(String name, String age, String policyType, String[] options) {
         super(name, age, policyType, options);
     }
 
     @Override
     public void calculateBasePremium() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculateBasePremium'");
+        if (isRented == true) {
+            basePremium = (int) (sumInsured * 0.02);
+        } else {
+            basePremium = (int) (sumInsured * 0.01);
+        }
     }
 }
